8e0d6301da351b02f66575c0af706b51358d2039
Prasanth-Reddy-K <129927291+Prasanth-Reddy-K@users.noreply.github.com>
2024-06-30 15:53:31 +0530

    Add files via upload

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    flowbite.content(),
+  ],
+  plugins: [
+    // ...
+    flowbite.plugin(),
+    require('tailwind-scrollbar'),
+  ],
+};
+
